0431e3f73fcd8531cea929b7300039629eb9311e
augusto <augusto.pinochet@alumnos.uv.cl>
2022-11-27 20:30:10 -0300

    carpetas

---
+++ node_project/controllers/sharks.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const Shark = require('../models/sharks');
+
+exports.index = function (req, res) {
+    res.sendFile(path.resolve('views/sharks.html'));
+};
+
+exports.create = function (req, res) {
+    var newShark = new Shark(req.body);
+    console.log(req.body);
+    newShark.save(function (err) {
+            if(err) {
+            res.status(400).send('Unable to save shark to database');
+        } else {
+            res.redirect('/sharks/getshark');
+        }
+  });
+               };
+
+exports.list = function (req, res) {
+        Shark.find({}).exec(function (err, sharks) {
+                if (err) {
+                        return res.send(500, err);
+                }
+                res.render('getshark', {
+                        sharks: sharks
+             });
+        });
+};
+
+exports.delete = function (req, res) {
+        Shark.remove((Shark)),
+        function(err, data){
+                if(err){
+                        return res.send(500, err);
+                }
+        else{
+                res.send(data);
+        }
+}}
---
+++ node_project/routes/sharks.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const shark = require('../controllers/sharks');
+
+router.get('/', function(req, res){
+    shark.index(req,res);
+});
+
+router.post('/addshark', function(req, res) {
+    shark.create(req,res);
+});
+
+router.get('/getshark', function(req, res) {
+    shark.list(req,res);
+});
+
+router.post('/deleteshark', function(req, res) {
+    shark.delete(req,res);
+});
+
+module.exports = router;
